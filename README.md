8a5484a32ca7f8bacc28bc0391b5fec12e455784
Marcelo Valderrama <marcelovalderramacorrea@MacBook-Air-de-Marcelo.local>
2023-08-31 18:02:06 -0300

    Commit do Ignite

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # ignite2023
+
+```
+docker run --name api-solid-pg -e  -e  -e  -p 5432:5432 bitnami/postgresql
+```
+
+```
+docker compose up -d
+
+```
+
+
+```
+docker-compose down
+```
---
+++ src/http/controller/register.ts
@@ -0,0 +1,30 @@
+import { FastifyRequest, FastifyReply } from 'fastify'
+import { prisma } from '@/lib/prisma'
+import { z } from 'zod'
+import { hash } from 'bcryptjs'
+import { RegisterUseCase } from '@/use-cases/register'
+import { PrismaUsersRepository } from '@/repositories/prisma-users-repository'
+
+export async function register(request: FastifyRequest, reply: FastifyReply) {
+  const registerBodySchema = z.object({
+    name: z.string(),
+    email: z.string(),
+    password: z.string().min(6),
+  })
+
+  const { name, email, password } = registerBodySchema.parse(request.body)
+
+  try {
+    const prismaUsersRepository = new PrismaUsersRepository()
+    const registerUseCase = new RegisterUseCase(prismaUsersRepository)
+    await registerUseCase.execute({
+      name,
+      email,
+      password,
+    })
+  } catch (error) {
+    return reply.status(409).send()
+  }
+
+  return reply.status(201).send()
+}
